b042429f072967a6568bb976293265f7fb7be11c
Craig Jensen1 <craig.jensen1@snow.edu>
2026-02-24 09:58:17 -0700

    Add project files.

---
+++ ConsoleApp5/Quest.cs
@@ -0,0 +1,29 @@
+﻿namespace QuestProgressTracker
+{
+    public class Quest
+    {
+        private string v;
+
+        public Quest(string v)
+        {
+            this.v = v;
+        }
+
+        public bool IsCompleted { get; set; }
+
+        public void AddObjective(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetObjective(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProgressObjective(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
